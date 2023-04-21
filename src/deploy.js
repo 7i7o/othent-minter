8925fb5ad71cf19d74b6cf653ae8f6e7b0119de3
Tate Berenbaum <tateflyer1@gmail.com>
2023-04-21 00:22:46 -0400

    feat: deploy script WIP

--- src/deploy.js
+++ src/deploy.js
@@ -8,3 +8,92 @@
  * 2.B) Send an email to the user with the template provided by Fayaz
  * 3) Celebrate 🎉
  */
+
+import nodemailer from 'nodemailer';
+import fs from "fs";
+import path from 'path';
+import Arweave from "arweave";
+
+const emailFrom = "hello@othent.io";
+const emailPassword = "PASSWORD GOES HERE";
+const emailTemplate = fs.readFileSync(path.resolve(__dirname, "./templates/email.html"));
+const contractSource = "CONTRACT SOURCE HERE";
+const JWK = {};
+
+async function parseUserList() {
+
+}
+
+async function mintAsset(userAddress) {
+  const client = await Arweave.init({
+    host: 'arweave.net',
+    port: 443,
+    protocol: 'https'
+  });
+
+  const sbt = await client.createTransaction({
+    data: "CERTIFICATE IMAGE DATA HERE"
+  }, JWK);
+  sbt.addTag("App-Name", "SmartWeaveContract");
+  sbt.addTag("App-Version", "0.3.0");
+  sbt.addTag("Contract-Src", contractSource);
+  sbt.addTag("Content-Type", "image/JPEG");
+  sbt.addTag("Contract-State", JSON.stringify({
+    balances: {
+      userAddress: 1
+    }
+  }));
+
+  await client.transactions.sign(sbt);
+  await client.transactions.post(sbt);
+}
+
+async function sendEmail(userEmail) {
+  const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: emailFrom,
+      pass: emailPassword,
+    },
+  });
+  const outboundEmail = {
+    from: emailFrom,
+    to: userEmail,
+    subject: 'Arweave Frontier NFT',
+    html: emailTemplate
+  };
+  transporter.sendMail(outboundEmail, (error, info) => {
+    if (error) {
+      console.log(error, info);
+      throw new Error(error);
+    } else {
+      return 'Email sent';
+    }
+  });
+}
+
+async function runIt() {
+  // userList type should be: { address: string, email: string }[]
+  const userList = await parseUserList();
+  for (let i = 0; i < userList.length; i++) {
+    console.log(`Now minting for user: ${userList[i].address}`);
+    let txID;
+    try {
+      txID = await mintAsset(userList[i].address);
+    } catch (err) {
+      throw new Error(err);
+    }
+
+    console.log(`Now emailing for user: ${userList[i].address}`);
+    try {
+      const confirmation = await sendEmail(userList[i].email);
+    } catch (err) {
+      throw new Error(err);
+    }
+
+    setTimeout(() => { console.log("Sleeping to avoid DDOSing mail servers") }, 1000);
+  }
+}
+
+// Bombs away
+runIt();
